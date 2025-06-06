f487569ccb9a0fc7c284b1933546c520a68f7b57
dayul <dyulchoo@naver.com>
2025-06-06 12:44:43 +0900

    ✨ [feat] : 응답 DTO 및 응답 메세지 상수 정의, 데코레이터 구현

---
+++ src/common/constants/message.ts
@@ -0,0 +1,10 @@
+export const MESSAGE = {
+  USER_EXISTS: 'User already exists',
+  USER_CREATED: 'User created successfully',
+  INVALID_CREDENTIALS: 'Invalid credentials',
+  USER_NICKNAME_FOUND: 'User nickname fetched successfully',
+  USER_FOUND: 'User fetched successfully',
+  USER_NICKNAME_NOT_FOUND: 'User nickname not found',
+  USER_NICKNAME_UPDATED: 'User nickname updated',
+  USER_NOT_FOUND: 'User not found',
+};
---
+++ src/common/decorators/current-user-id.decorator.ts
@@ -0,0 +1,9 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { UserDto } from 'src/auth/dto/user.dto';
+
+export const CurrentUserId = createParamDecorator(
+  (data: unknown, ctx: ExecutionContext) => {
+    const request = ctx.switchToHttp().getRequest<{ user?: UserDto }>();
+    return request.user?.userId;
+  },
+);
