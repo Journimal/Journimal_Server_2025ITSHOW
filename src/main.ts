9d2481bcc1ab8248f4db97ede809cc6e66b40915
dayul <dyulchoo@naver.com>
2025-06-15 22:43:21 +0900

    🚀 [deploy] : cors 설정

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // CORS 허용 설정 (flutter는 상관 없음)
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
